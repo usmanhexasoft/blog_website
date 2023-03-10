a2fa8e8dec8cb0e753c71cf1594af25761399e38
muhammad <musmanbhutta50929@gmail.com>
2023-03-10 11:49:03 +0500

    Initial commit

---
+++ adminstats/db.php
@@ -0,0 +1,8 @@
+<?php
+$con=mysqli_connect("localhost","root","admin123","blog_admin_db");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+ ?>
---
+++ database/conn.php
@@ -0,0 +1,4 @@
+<?php
+//database connection
+($GLOBALS["___mysqli_ston"] = mysqli_connect("localhost","root","admin123","blog_admin_db"));  //host,user,password,database
+?>
